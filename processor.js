5a263246bb6bcddac1b4b6b6b93e7c3c1c5f2fa1
Visweshwaran Suryanarayanan <vsuryanarayanan@sapient.com>
2016-12-22 16:05:26 +0000

    GREEN - First try to get the test cases passed.

--- processor.js
+++ processor.js
@@ -1,3 +1,32 @@
 /**
  * Created by vsury1 on 12/22/16.
  */
+var constants = require('./constants');
+
+function divisibleBy(input, divisor) {
+  return ((input % divisor) === 0);
+}
+
+function fizzOrBuzz(input) {
+
+  switch (input) {
+    case (divisibleBy(input, 3)):
+      return constants.FIZZ;
+      break;
+
+    case (divisibleBy(input, 5)):
+      return constants.BUZZ;
+
+    case (divisibleBy(input, 3) && divisibleBy(input, 5)):
+      return constants.FIZZBUZZ;
+
+    default:
+      return input;
+
+  }
+
+}
+
+module.exports = {
+  fizzOrBuzz: fizzOrBuzz
+};
